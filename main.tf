8a27b5c7bfe31ec7ca6b5fddf4335d3848be2311
Eran Krakovsky <ekrako@gmail.com>
2023-09-10 14:16:10 +0300

    refactor to use variables

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "app_ami" {
 
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = [var.ami_filters.filter]
   }
 
   filter {
@@ -11,23 +11,23 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = ["979382823631"] # Bitnami
+  owners = [var.ami_filters.owner] 
 }
 
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "dev"
-  cidr = "10.0.0.0/16"
+  name = var.env
+  cidr = "${var.env.network_prefix}.0.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets  = ["${var.env.network_prefix}.101.0/24", "${var.env.network_prefix}.102.0/24", "${var.env.network_prefix}.103.0/24"]
   # private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   # enable_nat_gateway = true
 
   tags = {
     Terraform = "true"
-    Environment = "dev"
+    Environment = var.env.name
   }
 }
 
@@ -36,7 +36,7 @@ module "alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 8.0"
 
-  name = "blog-alb"
+  name = "${var.env.name}-blog-alb"
 
   load_balancer_type = "application"
 
@@ -46,7 +46,7 @@ module "alb" {
 
    target_groups = [
     {
-      name_prefix      = "blog-"
+      name_prefix      = "${var.env.name}-"
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
@@ -62,7 +62,7 @@ module "alb" {
   ]
 
   tags = {
-    Environment = "dev"
+    Environment = var.env.name
   }
 }
 
@@ -71,9 +71,9 @@ module "autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "6.10.0"
   
-  name          = "blog"
-  min_size      = 1
-  max_size      = 2
+  name          = "${var.env.name}-blog"
+  min_size      = var.min_size
+  max_size      = var.max_size
   image_id      = data.aws_ami.app_ami.id
   instance_type = var.instance_type
   
@@ -83,7 +83,7 @@ module "autoscaling" {
 }
 
 resource "aws_security_group" "blog" {
-  name        = "blog"
+  name        = "${var.env.name}-blog"
   description = "Allow http and https"
 
   vpc_id = module.vpc.vpc_id
--- variables.tf
+++ variables.tf
@@ -2,3 +2,40 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
 }
+
+variable "ami_filters" {
+  description = "owner and filter of the ami to look for"
+  type = object({
+    filter  = string
+    owner   = string
+  })
+  
+  default = {
+    filter  = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
+    owner   = "979382823631" # Bitnami
+  } 
+}
+
+variable "env" {
+  description = "the enviroment of the deployment"
+
+  type = object({
+    name           = string
+    network_prefix = string
+  })
+  
+  default = {
+    name            = "dev"
+    network_prefix = "10.0"
+  }
+}
+
+
+variable "min_size" {
+  description = "minimum number of instances"
+  default = 1
+}
+variable "max_size" {
+  description = "maximum number of instances"
+  default = 2
+}
