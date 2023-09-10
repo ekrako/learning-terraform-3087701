31ff9d7593d4efa792159662f406a053561bb780
Eran Krakovsky <ekrako@gmail.com>
2023-09-10 11:11:51 +0300

    add dev

--- main.tf
+++ main.tf
@@ -49,7 +49,7 @@ resource "aws_security_group" "blog" {
   name        = "blog"
   description = "Allow http and https"
 
-  vpc_id = module.vpc.id
+  vpc_id = module.vpc.dev.id
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
