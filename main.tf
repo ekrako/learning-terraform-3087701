493acd511b9aa4d746fd28c6257f000d345bf8f4
Eran Krakovsky <ekrako@gmail.com>
2023-09-10 11:46:24 +0300

    load balancer

--- main.tf
+++ main.tf
@@ -32,6 +32,46 @@ module "vpc" {
 }
 
 
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 8.0"
+
+  name = "blog-alb"
+
+  load_balancer_type = "application"
+
+  vpc_id             = module.vpc.vpc_id
+  subnets            = module.vpc.public_subnets
+  security_groups    = [aws_security_group.blog.id]
+
+   target_groups = [
+    {
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+      targets = {
+        my_target = {
+          target_id = aws_instance.blog.id
+          port = 80
+        }
+      }
+    }
+  ]
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+  tags = {
+    Environment = "dev"
+  }
+}
+
 
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
@@ -80,3 +120,4 @@ resource "aws_security_group_rule" "all_out" {
 
   security_group_id = aws_security_group.blog.id
 }
+
