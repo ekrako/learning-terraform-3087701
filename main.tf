b103ecfd88a540b7060f141cc6b806b36fe842b9
Eran Krakovsky <ekrako@gmail.com>
2023-09-10 11:34:45 +0300

    fixed issues

--- main.tf
+++ main.tf
@@ -48,7 +48,7 @@ resource "aws_security_group" "blog" {
   name        = "blog"
   description = "Allow http and https"
 
-  vpc_id = module.vpc.id
+  vpc_id = module.vpc.vpc_id
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
